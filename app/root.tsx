f9a28decd96ed5b96bbecb2cb62f6366e8fb2620
fabischkamau <126986727+fabischkamau@users.noreply.github.com>
2024-04-15 18:15:29 +0300

    theme

--- app/root.tsx
+++ app/root.tsx
@@ -29,9 +29,9 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
 function AppLayout({ children }: { children: React.ReactNode }) {
   const data = useLoaderData<typeof loader>();
-  const [theme] = useTheme();
+
   return (
-    <html lang="en" className={clsx(theme)}>
+    <html lang="en" className={clsx(data.theme)}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
